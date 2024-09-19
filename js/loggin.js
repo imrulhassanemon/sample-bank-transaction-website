5dde9b98307c5dafb7fff3a140cdc1f0cbfecfb0
imrulhassanemon <imrulhassan989@gmail.com>
2024-09-20 02:26:52 +0600

    make transaction website

---
+++ js/loggin.js
@@ -0,0 +1,7 @@
+// console.log("buddon")
+
+
+document.getElementById("loggin-btn").addEventListener("click", function(event){
+    event.preventDefault(); //form submit reloading the page
+    console.log("button just cliked")
+})
