f6c55d995753eda306a4eaabf515e68343313e09
imrulhassanemon <imrulhassan989@gmail.com>
2024-09-20 04:08:45 +0600

    added home page

--- js/loggin.js
+++ js/loggin.js
@@ -1,7 +1,49 @@
-// console.log("buddon")
+// // console.log("buddon")
+
+// // step-1 add Eventhandaler 
+// document.getElementById("loggin-btn").addEventListener("click", function(event){
+//     // step-2 prevent the auto brawser reloding 
+//     event.preventDefault(); //form submit reloading the page
+
+//     // step-3 get the phone number 
+
+
+//     const phoneNumber = document.getElementById("phone-number").value;
+//     // console.log(phoneNumber)
+    
+//     // step-4 get the pin number 
+
+//     const pinNumber = document.getElementById("pin-number").value;
+//     console.log(pinNumber, phoneNumber)
+
+
+
+// })
+
+
+
+
+// new code for practice 
 
 
 document.getElementById("loggin-btn").addEventListener("click", function(event){
-    event.preventDefault(); //form submit reloading the page
-    console.log("button just cliked")
+    // step-1 stop browser relod
+    event.preventDefault();
+
+    // step-2 get the number 
+    const phoneNumber = document.getElementById("phone-number").value;
+    console.log(phoneNumber)
+
+    const pinNumber = document.getElementById("pin-number").value;
+    console.log(pinNumber)
+
+
+    // this phone number and pin number is temporarry bad way to validate the pin and pass
+    if(phoneNumber === "5" && pinNumber === '123'){
+        console.log("your pass is correct")
+        window.location.href = "home.html"
+    }else{
+        alert("your mobail number or password is incorrect is incorrect please enter the valid pass or pin")
+    }
+
 })
